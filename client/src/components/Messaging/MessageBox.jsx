eb2736efecc68560d446cde8c917b15af1e7c59e
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-08 15:26:57 -0800

    [msg] moved to bottom, import global button

--- client/src/components/Messaging/MessageBox.jsx
+++ client/src/components/Messaging/MessageBox.jsx
@@ -1,34 +1,42 @@
 import React, { Component } from 'react'
 import io from 'socket.io-client';
 
+import ChatList from './ChatList.jsx'
+import Button from '../globals/Button'
+
 class MessageBox extends Component {
   constructor() {
     super();
     this.state = {
       user: '',
       message: '',
+      allChats: [],
     }
   }
 
   componentDidMount() {
+    // this.state.allChats.forEach(chat => {
 
+    // })
   }
 
-  createUsername = () => {
-    socket.emit('createUsername') //grab the username 
-  }
+  // createUsername = () => {
+  //   socket.emit('createUsername') //grab the username 
+  // }
 
-  sendMessage = (message) => {
+  sendMessage = (messageToSend) => {
     let socket = io('http://localhost:4155');
     const ownerEmail = localStorage.getItem('email');
-    if (message && ownerEmail) {
-      socket.emit('message', { message: message, user: ownerEmail })
+    if (messageToSend && ownerEmail) {
+      socket.emit('message', { message: messageToSend, user: ownerEmail })
+    } else {
+      alert('Please include a message.')
     }
     socket.on('newMessage', data => {
-      this.setState({
-        user: data.user,
-        message: data.message,
-      })
+      console.log('data', data)
+      this.state.allChats.push(data)
+      console.log('this.state.allChats', this.state.allChats)
+      // this.setState({ user: data.user, message: data.message })
     })
   }
 
@@ -44,18 +52,35 @@ class MessageBox extends Component {
     return (
       <div className="messaging-box">
         <div className="display-message">
-          {this.state.user} {this.state.message}
           <div className="message-bar">
             <input id="text" type="text" onChange={this.onTextChangeHandler} />
-            <div id="chat-btn">
-              <button type="button" onClick={this.buttonClickHandler}>Click</button>
-            </div>
+            {/* <button id="chat-btn" type="button" onClick={this.buttonClickHandler}>Send</button> */}
+            <Button
+              backgroundColor="red"
+              color="white"
+              text="send"
+              onClick={this.buttonClickHandler}
+            />
           </div>
         </div>
         <style>{`
         #text {
           width: 225px;
           height: 10px;
+          font-size: 12px;
+          padding: 15px 0px 15px 8px;;
+        }
+        .messaging-box {
+          position: fixed;
+          bottom:0
+        }
+        .message-bar {
+          display: flex;
+        }
+        #chat-btn {
+          font-size: 12px;
+          font-color: white;
+          background-color:red;
         }
         `}</style>
       </div>
