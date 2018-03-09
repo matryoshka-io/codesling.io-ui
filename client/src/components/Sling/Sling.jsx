efe12de8b4aacc59d8b59c9c540f6f3c60a367f4
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-08 19:25:39 -0800

    [msg] add props to pass down

--- client/src/components/Sling/Sling.jsx
+++ client/src/components/Sling/Sling.jsx
@@ -109,7 +109,7 @@ class Sling extends Component {
             color="white"
             onClick={() => this.submitCode()}
           />
-          <MessagingIndex />
+          <MessagingIndex socket={this.props.socket} />
         </div>
         <div className="code2-editor-container">
           <CodeMirror
