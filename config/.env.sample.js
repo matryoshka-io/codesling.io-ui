65276f2607181639963510031025aeb0f3490920
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-09 19:15:26 -0800

    [ops] fixed server port

--- config/.env.sample.js
+++ config/.env.sample.js
@@ -2,27 +2,27 @@ const envBuild = {
   development: {
     server: ["PORT=1337"],
     client: [
-      "NODE_ENV=DEVELOPMENT",
+      "NODE_ENV=development",
       "DEBUG=TRUE",
       "ENVPREFIX=REACT_APP_",
-      "REST_SERVER_URL=http://localhost:4990",
+      "REST_SERVER_URL=http://localhost:3396",
       "SOCKET_SERVER_URL=http://localhost:4155",
       "CODERUNNER_SERVICE_URL=http://localhost:4000",
       "REACT_APP_SOCKET_SERVER_URL=http://localhost:4155",
-      "REACT_APP_REST_SERVER_URL=http://localhost:4990"
+      "REACT_APP_REST_SERVER_URL=http://localhost:3396"
     ]
   },
   production: {
     server: ["PORT=1337"],
     client: [
-      "NODE_ENV=PRODUCTION",
+      "NODE_ENV=production",
       "DEBUG=FALSE",
       "ENVPREFIX=REACT_APP_",
-      "REST_SERVER_URL=http://localhost:4990",
+      "REST_SERVER_URL=http://localhost:3396",
       "SOCKET_SERVER_URL=http://localhost:4155",
       "CODERUNNER_SERVICE_URL=http://localhost:4000",
       "REACT_APP_SOCKET_SERVER_URL=http://localhost:4155",
-      "REACT_APP_REST_SERVER_URL=http://localhost:4990"
+      "REACT_APP_REST_SERVER_URL=http://localhost:3396"
     ]
   }
 };
