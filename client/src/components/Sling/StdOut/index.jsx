3a92e2981f174f690d498492eea0531e78afeb58
James Yen <jameshyen@gmail.com>
2018-03-10 15:45:13 -0800

    Lint

--- client/src/components/Sling/StdOut/index.jsx
+++ client/src/components/Sling/StdOut/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import Loading from '../../globals/Loading';
 
 const StdOut = ({ text }) => (
   <div>
     {text.split('\n').map((singleLine, idx) => (
-      <div key={`stdout-singleline-idx-${idx}`}><i>{singleLine}</i></div>
+      <div key={`stdout-singleline-idx-${idx}`} /* eslint-disable-line */ ><i>{singleLine}</i></div>
     ))}
   </div>
 );
+
+StdOut.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default StdOut;
