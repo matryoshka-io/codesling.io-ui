847344feab58f31ce957a5319a5a30e8673a6bff
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-10 11:59:13 -0800

    include a log out button, doesn't do anything

--- client/src/components/Sling/EditorHeader/EditorNavbar.js
+++ client/src/components/Sling/EditorHeader/EditorNavbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../globals/Button/index.js';
 
 const EditorNavbar = () => (
   <nav className="editor-navbar">
@@ -7,6 +8,11 @@ const EditorNavbar = () => (
       <li>Item 2</li>
       <li>Item 3</li>
     </ul>
+    <Button
+      backgroundColor="red"
+      color="white"
+      text="Logout"
+    />
   </nav>
 );
 
